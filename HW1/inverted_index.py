b038cf367d2120ad279de0e8de88ff221e94ce1e
jshen1s1 <40844558+jshen1s1@users.noreply.github.com>
2022-05-01 21:17:25 -0700

    Add relevance feedback

--- HW1/inverted_index.py
+++ HW1/inverted_index.py
@@ -1,5 +1,3 @@
-from tracemalloc import stop
-import nltk
 import math
 from collections import defaultdict
 from collections import Counter
@@ -9,11 +7,12 @@ from nltk.stem import snowball
 
 ### tokenize the documents
 doc = []
-
+docNames = []
 with open('../ohsumed.88-91','r') as f:
     lines = f.readlines()
     for i,line in enumerate(lines):
         if line.strip() == ".W":
+            docNames.append(int(lines[i-9].strip()))
             doc.append(lines[i+1].strip())
 
 doc_token = defaultdict(set) # stores word and its corresponding document ids
@@ -23,7 +22,7 @@ stwords = set(stopwords.words('english'))
 # word stemming
 stemmer = snowball.SnowballStemmer('english')
 
-for docid, c in enumerate(doc[:10]):
+for docid, c in zip(docNames, doc):
     for sentence in sent_tokenize(c):
         for word in word_tokenize(sentence):
             word = word.lower()
@@ -35,10 +34,11 @@ for docid, c in enumerate(doc[:10]):
 
 
 id_word_freq = defaultdict(list) # stores the frequency of each word in each document
-for i in range(len(docid_word)):
+for i in docid_word:
     id_word_freq[i] = Counter(docid_word[i])
 
-#print(len(docid_word[0]))
+#print(doc_token)
+#print(id_word_freq)
 
 ### parse query file
 query_content = []
@@ -83,9 +83,7 @@ def Boolean():
     for queryid in query_inverted_index:
         scores = defaultdict(list)
         query_terms = query_inverted_index.get(queryid)
-        #print(query_inverted_index.get(query_id))
         for word in query_terms:
-            #print(query_terms[word])
             for docid in query_terms[word]:
                 if scores[docid]:
                     scores[docid] += 1
@@ -145,7 +143,53 @@ def TF_IDF():
 
     return final_scores
     
-print(TF_IDF())
+#print(TF_IDF())
 
 ### Relevence feedback
-#def pseudo_r_f:
+def Rocchio_r_f():
+    final_scores = defaultdict(list)
+    bata = 0.65
+
+    for queryid in query_inverted_index:
+        scores = defaultdict(list)
+        query_terms = query_inverted_index.get(queryid)
+        j = 0
+        for word in query_terms:
+            for docid in query_terms[word]:
+                if scores[docid]:
+                    scores[docid] += bata * math.log(weighted_term_frequency(word, docid) + 1) * computeIDF(word)
+                else:
+                    scores[docid] = bata* math.log(weighted_term_frequency(word, docid) + 1) * computeIDF(word)
+                j += 1
+                if j == 10:
+                    break
+            j = 0
+        soreted_key = sorted(scores, key=scores.get, reverse=1)
+        sorted_score = {}
+        for k in soreted_key:
+            sorted_score[k] = scores[k]
+        final_scores[queryid] = sorted_score
+
+    return final_scores
+
+# function write result to log file
+def dump(scores, fname):
+    with open(fname, "w") as newfile:
+        for queryid in scores:
+            rank = 1
+            for docid in scores[queryid]:
+                queryID = "OHSU"+str(queryid+1)
+                score = str(scores[queryid].get(docid))
+                newfile.write(queryID + " Q0 " + str(docid) + " " + str(rank) + " " + score + " tfidf-Both\n")
+                rank += 1
+            rank = 1
+
+boolean_rank = Boolean()
+TF_rank = TF()
+TF_IDF_rank = TF_IDF()
+r_f_rank = Rocchio_r_f()
+
+dump(boolean_rank, "boolean.txt")
+dump(TF_rank, "TF.txt")
+dump(TF_IDF_rank, "TF_IDF.txt")
+dump(r_f_rank, "RF.txt")
